754cf343e5a1949d9c4d82bb5289cf6b3ca6df9e
Eva Yu <35118281+evaqy@users.noreply.github.com>
2020-04-13 17:27:08 -0400

    rename file name

--- 12.Debug.py
+++ 12.Debug.py
@@ -1,4 +1,8 @@
 #1 请找出以下代码的错误，并修改使其正常运行
+#     age = input('请输入数字： ')
+#     if age == 10
+#         print('小明今年%d岁了'%age)
+#     print('程序结束')
 
 def age():
     age = int(input('请输入数字： '))
@@ -9,6 +13,13 @@ def age():
 age()
 
 #2 请修改以下代码的错误，并修改使其正常运行（代码实现对列表求和）
+# list = [1,2,3,4]
+
+# def run():
+#     he = sum(list)
+# print(he)
+# run()
+
 list = [1,2,3,4]
 
 def run():
--- 5.list & dictionary.py
+++ 5.list & dictionary.py
@@ -1,3 +1,14 @@
+#1
+mv_list = ['西红柿首富','银河护卫队','金刚狼','银河补习班','叶问','狮子王','钢铁侠']
+print(mv_list[2:5])
+
+del mv_list[4]
+print(mv_list)
+
+mv_list.append('蜘蛛侠')
+print(mv_list)
+
+#2
 phonebook = {'marry':123, 'peter':456, 'john':789}
 
 # 增加姓名和手机
@@ -16,4 +27,3 @@ print(list(phonebook.keys()))
 
 # 根据姓名查找手机号
 print(phonebook['peter'])
-
