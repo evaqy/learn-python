a82991cbeac33160944d08535270fe49d44b6cb7
Eva Yu <35118281+evaqy@users.noreply.github.com>
2020-05-07 10:43:26 -0400

    update file

--- Web Scraping/4.project - web scraping by using json.py
+++ Web Scraping/4.project - web scraping by using json.py
@@ -1,5 +1,7 @@
 #根据已给的⽹址分析⽹站特征，将最新发布的岗位第⼀⻚的标题例如（28297-（⾼级） 海外战略分析经理）爬取到本地。
 import requests
+from bs4 import BeautifulSoup
+import json
 
 start_url = "https://careers.tencent.com/tencentcareer/api/post/Query?timestamp=1585450579819&countryId=&cityId=&bgIds=&productId=&categoryId=&parentCategoryId=&attrId=&keyword=&pageIndex=1&pageSize=10&language=zh-cn&area=cn"
 
@@ -13,5 +15,4 @@ try:
             print(position_title)
 except Exception as error:
     print(error)
-
 
